d70a9f87f4390699e33845ceda2992e1f8c22098
Tohirul Islam <tohirul.islam164@gmail.com>
2025-05-17 15:33:14 +0600

    [Chore] tutorial 6

---
+++ tut-6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << "Check it out" << endl;
+
+    char name[5];
+    cout << "Enter your name: ";
+    cin >> name;
+
+    cout << "Hello!!! " << name << endl;
+    
+    return 0;
+}
