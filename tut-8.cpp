c0b7139140fef8af48d308f5d0a906ed5df85a77
Tohirul Islam <tohirul.islam164@gmail.com>
2025-05-17 15:53:52 +0600

    [Chore] tutorial 8

---
+++ tut-8.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    const int a = 56;
+     int b;
+
+     cout << "Enter the value for b: " << endl;
+     cin >> b;
+
+     cout << "SUM: " << a + b << endl;
+
+     return 0;
+}
