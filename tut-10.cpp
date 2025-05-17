b01e5c72c936bb9fe5358cd3f30ff50159e3080a
Tohirul Islam <tohirul.islam164@gmail.com>
2025-05-17 16:49:03 +0600

    [Chore] tutorial 10

---
+++ tut-10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int count = 5;
+
+    for (int i = 0; i < count; i++)
+    {
+        for (int j = 0; j <= i; j++)
+            cout << '*';
+        cout << endl;
+    }
+
+    return 0;
+}
