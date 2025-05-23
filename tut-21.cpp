5b1454f6792d6cd9314590720d9a54374cf4afb3
tohirul <tohirul.islam164@gmail.com>
2025-05-23 11:31:36 +0600

    [Chore]: files updated with new format

--- tut-21.cpp
+++ tut-21.cpp
@@ -1,46 +1,48 @@
 #include <iostream>
 using namespace std;
 
-class Student {
-private:
-  double grade, credit, fee;
-  string registration;
+class Student
+{
+  private:
+    double grade, credit, fee;
+    string registration;
 
-public:
-  string name, className, roll;
+  public:
+    string name, className, roll;
 
-  void setStudentInfo(string studentName, string studentClassName,
-                      string studentRoll) {
-    name = studentName;
-    className = studentClassName;
-    roll = studentRoll;
-  }
-  void setAcademicInfo(double studentGrade, double studentCredit,
-                       double studentFee);
-  void getStudentInfo() const {
-    cout << "Student Name: " << name << endl;
-    cout << "Class: " << className << endl;
-    cout << "Roll No: " << roll << endl;
-  }
-  void getStudentAcademicData() const {
-    cout << "Grade: " << grade << endl;
-    cout << "Credit: " << credit << endl;
-  };
+    void setStudentInfo(string studentName, string studentClassName, string studentRoll)
+    {
+        name      = studentName;
+        className = studentClassName;
+        roll      = studentRoll;
+    }
+    void setAcademicInfo(double studentGrade, double studentCredit, double studentFee);
+    void getStudentInfo() const
+    {
+        cout << "Student Name: " << name << endl;
+        cout << "Class: " << className << endl;
+        cout << "Roll No: " << roll << endl;
+    }
+    void getStudentAcademicData() const
+    {
+        cout << "Grade: " << grade << endl;
+        cout << "Credit: " << credit << endl;
+    };
 };
-void Student::setAcademicInfo(double studentGrade, double studentCredit,
-                              double studentFee) {
-  grade = studentGrade;
-  credit = studentCredit;
-  fee = studentFee;
+void Student::setAcademicInfo(double studentGrade, double studentCredit, double studentFee)
+{
+    grade  = studentGrade;
+    credit = studentCredit;
+    fee    = studentFee;
 };
 
-int main() {
+int main()
+{
+    Student one;
+    one.setStudentInfo("Tohirul Islam", "40-A", "A-2348");
+    one.getStudentInfo();
 
-  Student one;
-  one.setStudentInfo("Tohirul Islam", "40-A", "A-2348");
-  one.getStudentInfo();
-
-  one.setAcademicInfo(3.19, 12.5, 24536.00);
-  one.getStudentAcademicData();
-  return 0;
+    one.setAcademicInfo(3.19, 12.5, 24536.00);
+    one.getStudentAcademicData();
+    return 0;
 }
